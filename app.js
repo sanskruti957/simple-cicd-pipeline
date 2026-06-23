145824d91249608a2293d383e7d07109bd334383
Sanskruti Sahane <sanskrutisahane290@gmail.com>
2026-06-23 22:56:37 +0530

    Test automatic deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Sanskruti!');
+  res.send("CI/CD Pipeline Working Successfully!");
 });
 
 app.listen(3000, () => {
