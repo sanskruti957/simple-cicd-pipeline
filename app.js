e8f19ba01acb85d10217bf6ad03fa938d35e65b8
Sanskruti Sahane <sanskrutisahane290@gmail.com>
2026-06-25 13:21:21 +0530

    Final demo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("CI/CD Pipeline Demo Successful!");
+  res.send("Persevex Internship Project Demo!");
 });
 
 app.listen(3000, () => {
